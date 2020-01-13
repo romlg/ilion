3545c270a1a9ffb6f1c0c4617b469fb08d1e76dc
Vasiliy Antufev <vasiliyantufev@gmail.com>
2020-01-13 21:39:35 +0300

    service

---
+++ app/Helpers/Parse/ExcelParser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Helpers\ExcelParser;
+
+class ExcelParser {
+    /**
+     * @param int $user_id User-id
+     *
+     * @return string
+     */
+    public static function get_test() {
+        return 'testNew';
+    }
+}
