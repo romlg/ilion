468afe337f18b225789efde717499829b1753d6f
Vasiliy Antufev <vasiliyantufev@gmail.com>
2020-01-17 12:19:39 +0300

    Specifications SpecUnits migration

---
+++ database/migrations/2020_01_17_090210_spec_units.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SpecUnits extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('spec_units', function (Blueprint $table) {
+            $table->increments('sunit_id');
+            $table->integer('spec_id');
+            $table->integer('n_id');
+            $table->integer('count');
+            $table->integer('ver');
+            $table->tinyInteger('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('spec_units');
+    }
+}
