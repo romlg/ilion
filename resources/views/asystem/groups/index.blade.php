952fbdd087094ebaa9a0c42a6a5beea8e0ff90e6
Walle <vasiliyantufev@gmail.com>
2020-03-15 14:45:24 +0300

    form-group

--- resources/views/asystem/groups/index.blade.php
+++ resources/views/asystem/groups/index.blade.php
@@ -4,9 +4,10 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+
+                <div class="form-group">
                     <a class="btn btn-primary" href="{{ route('group.create') }}">Добавить</a>
-                </nav>
+                </div>
 
                 <div class="card">
                     <div class="card-body">
